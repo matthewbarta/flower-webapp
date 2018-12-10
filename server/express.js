c6fa2c95387658353b8ba0e9aa3fdf01750be52c
Matthew Barta <matthewbartaa@gmail.com>
2018-12-09 19:22:12 -0500

    Added database and HTML files

--- server/express.js
+++ server/express.js
@@ -1,25 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite = require('sqlite3');
 var path = require('path');
 const app = express();
 const port = 8080;
+
+const db = new sqlite.Database('/Users/matthewbarta/Desktop/final/server/db/flowers.db', (err) => console.log());
 
 module.exports.init = function() {
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join('/Users/matthewbarta/Desktop/final/client/index.html'));
+    db.serialize(() => {
+        db.each(`SELECT COMNAME as name
+                 FROM FLOWERS`, (err, row) => {
+          if (err) {
+            console.error(err.message);
+          }
+          console.log(row.name);
+        });
+      });
 });
 
 app.post('/', function (req, res) {
     res.send('Got a POST request')
 });
 
-app.put('/user', function (req, res) {
+app.put('/', function (req, res) {
     res.send('Got a PUT request at /user')
 });
 
-app.delete('/user', function (req, res) {
-    res.send('Got a DELETE request at /user')
+app.delete('/', function (req, res) {
+    res.send('Got a DELETE request at /')
 });
 
  //Serve static files
