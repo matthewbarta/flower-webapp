572c8ace1f26e1da599149616c50638aa4cf9477
Matthew Barta <matthewbartaa@gmail.com>
2018-12-11 04:55:31 -0500

    Added user logins

--- client/js/controllers/flowerController.js
+++ client/js/controllers/flowerController.js
@@ -1,5 +1,7 @@
 angular.module('flowers').controller('FlowersController', ['$scope', 'Flowers',
   function ($scope, Flowers) {
+    $scope.sightingInfo = {};
+    $scope.flowerInfo = {};
     /* Get all the flowers, then bind it to the scope */
     Flowers.getAll().then(function (response) {
       $scope.flowers = response.data;
@@ -15,6 +17,68 @@ angular.module('flowers').controller('FlowersController', ['$scope', 'Flowers',
         sightings: flower.sightings
       };
     };
+
+        //Adding a classroom
+        $scope.addSighting = function (index, place) {
+
+          //Accounts for filtered data
+          index = $scope.listings.indexOf(place);
+    
+          //Ensure the user is logged in prior to them adding a classroom.
+          var user = firebase.auth().currentUser;
+          if(user != null) {
+            var roomInfo = $scope.roomInfo;
+    
+            console.log(roomInfo);
+            if(roomInfo.roomNumber == null || roomInfo.outlets == null || roomInfo.description == null || roomInfo.description == '') {
+              window.alert("Must fill in all the fields to add a classroom!");
+              return;
+            }
+    
+            var duplicateRoom = false;
+      
+            //Check if the room number already exists.
+            for (var i = 0; i < $scope.listings[index].classRoomArray.length; i++) {
+              if ($scope.listings[index].classRoomArray[i].roomNumber == roomInfo.roomNumber) {
+                duplicateRoom = true;
+                break;
+              }
+            }
+      
+            //If it exists, reset the request and return.
+            if (duplicateRoom) {
+              window.alert("Cannot add duplicate room number!");
+              $scope.roomInfo = {rating: {}}; //Clear the scope afterwards.
+              $scope.roomInfo.roomSize = "Small"; //And set the small box to be checked.
+              $scope.roomInfo.blackboard = false;
+              $scope.roomInfo.whiteboard = false;
+              $scope.roomInfo.isOccupied = false;
+              return;
+            }
+            //Otherwise add the classroom the building's classroom array.
+            else {
+              $scope.listings[index].classRoomArray.push(roomInfo);
+      
+              Listings.update($scope.listings[index]._id, $scope.listings[index]).then(function (response) { }, function (error) {
+                console.log('Unable to update listing:', error);
+              });
+      
+              $scope.roomInfo = {rating: {}}; //Clear the scope afterwards.
+              $scope.roomInfo.roomSize = "Small"; //And set the small box to be checked.
+              $scope.roomInfo.blackboard = false;
+              $scope.roomInfo.whiteboard = false;
+              $scope.roomInfo.isOccupied = false;
+    
+              window.alert("A new room has been added to : " + place.code);
+              location.reload();
+              }
+          }
+          else {
+            window.alert("You must be logged in to create a new classroom!");
+            location.reload();
+            return;
+          }
+        };
    
     //Search for classrooms in the search bar.
     $scope.searchFilterCustom = function (entry) {
---
+++ client/login.js
@@ -0,0 +1,72 @@
+firebase.auth().onAuthStateChanged(function(user) {
+	if (user) {
+    // User is signed in.
+    document.getElementById("in_div").style.display = "none";
+    document.getElementById("out_div").style.display = "initial";
+
+    var user = firebase.auth().currentUser;
+
+    //Dislay the name of the user logged in.
+    if(user != null) {
+
+      var email_id = user.email;
+      var email = email_id.split('@');
+
+	  document.getElementById("user_id").innerHTML = "Logged in as: " + email[0];
+    }
+
+} else {
+    // No user is signed in.
+    document.getElementById("in_div").style.display = "initial";
+	document.getElementById("out_div").style.display = "none";
+}
+});
+
+function login() {
+
+	var email = document.getElementById("email_field").value;
+	var password = document.getElementById("password_field").value;
+
+	firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+		document.location.href = "index.html";
+	}).catch(function(error) {
+  	// Handle Errors here.
+  	var errorCode = error.code;
+  	var errorMessage = error.message;
+  	window.alert("Error : " + errorMessage);
+	});
+}
+
+function logout() {
+	firebase.auth().signOut().then(function() {
+  		// Sign-out successful.
+  		document.location.href = "index.html";
+  	}).catch(function(error) {
+  		// An error happened.
+  		var errorMessage = error.message;
+  		window.alert("Error : " + errorMessage);
+  	});
+
+  }
+
+  function createAccount() {
+
+  	var email = document.getElementById("new_email_field").value;
+  	var password = document.getElementById("new_password_field").value;
+
+  	var indexOfAt = email.indexOf('@ufl.edu');
+  	if(indexOfAt == -1) {
+  		window.alert("Error : the email address used must be a UFL email (@ufl.edu)");
+  		return;
+  	}
+
+  	firebase.auth().createUserWithEmailAndPassword(email, password).then(function() {
+  		document.location.href = "index.html";
+  	}).catch(function(error) {
+  		// Handle Errors here.
+  	var errorCode = error.code;
+  	var errorMessage = error.message;
+ 		 // ...
+ 	 window.alert("Error : " + errorMessage);
+	});
+  }
